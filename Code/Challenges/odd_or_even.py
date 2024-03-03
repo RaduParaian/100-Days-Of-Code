deba16ad0a929ccdfb3d8e5064dc4725586905f3
Radu <radu.g.paraian@gmail.com>
2024-03-03 10:40:24 +0000

    Changed Directory

---
+++ Code/Challenges/odd_or_even.py
@@ -0,0 +1,8 @@
+print("Welcome to the Odd or Even Number Checker!")
+
+number = int(input("Enter a number. \n"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
